ac64485d1ab67e6c020ae21df4195f002d33612c
Ms.P <u22524798@tuks.co.za>
2024-06-02 09:37:51 +0200

    added Userlogin tests

---
+++ src/Backend/src/test/kotlin/com/devforce/backend/controllers/UserControllerTest.kt
@@ -0,0 +1,16 @@
+package com.devforce.backend.controllers
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class UserControllerTest {
+
+    @Test
+    fun registerUser() {
+    }
+
+    @Test
+    fun loginUser() {
+    }
+}
