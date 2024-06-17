3cfab908507725390fba21b5caf6a3131615c6d6
Nokukhanya Ndlovu <135546695+NokukhanyaNdlovu@users.noreply.github.com>
2024-06-17 16:30:02 +0200

    Update EventRepo.kt

--- src/Backend/src/main/kotlin/com/devforce/backend/repo/EventRepo.kt
+++ src/Backend/src/main/kotlin/com/devforce/backend/repo/EventRepo.kt
@@ -1,13 +1,14 @@
 package com.devforce.backend.repo
 
 import com.devforce.backend.model.EventModel
+import com.devforce.backend.model.UserModel
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
+//import org.springframework.stereotype.Repository
 import java.util.*
 
-@Repository
+//@Repository
 interface EventRepo: JpaRepository<EventModel, UUID> {
-
+    fun findByAttendeesIs(user: UserModel): List<EventModel>
 }
 
 
