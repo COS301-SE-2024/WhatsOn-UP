bcdd78621f799c9e80cae35e44ade389e854dbf7
Nokukhanya Ndlovu <135546695+NokukhanyaNdlovu@users.noreply.github.com>
2024-06-17 16:29:58 +0200

    Update ElasticsearchConfig.kt

--- src/Backend/src/main/kotlin/com/devforce/backend/config/ElasticsearchConfig.kt
+++ src/Backend/src/main/kotlin/com/devforce/backend/config/ElasticsearchConfig.kt
@@ -1,6 +1,8 @@
 package com.devforce.backend.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 
 @Configuration
