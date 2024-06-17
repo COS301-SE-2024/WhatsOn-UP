598078964915e907ba0baa144e24ccfdac1b6745
Nokukhanya Ndlovu <135546695+NokukhanyaNdlovu@users.noreply.github.com>
2024-06-18 00:26:54 +0200

    elastic search changes

--- src/Backend/src/main/kotlin/com/devforce/backend/repo/EventRepo.kt
+++ src/Backend/src/main/kotlin/com/devforce/backend/repo/EventRepo.kt
@@ -3,12 +3,8 @@ package com.devforce.backend.repo
 import com.devforce.backend.model.EventModel
 import com.devforce.backend.model.UserModel
 import org.springframework.data.jpa.repository.JpaRepository
-//import org.springframework.stereotype.Repository
 import java.util.*
 
-//@Repository
 interface EventRepo: JpaRepository<EventModel, UUID> {
     fun findByAttendeesIs(user: UserModel): List<EventModel>
 }
-
-
--- src/Backend/src/main/kotlin/com/devforce/backend/service/EventService.kt
+++ src/Backend/src/main/kotlin/com/devforce/backend/service/EventService.kt
@@ -7,7 +7,6 @@ import com.devforce.backend.model.EventModel
 import com.devforce.backend.repo.EventRepo
 import com.devforce.backend.repo.elasticSearch.EventElasticsearchRepo
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
