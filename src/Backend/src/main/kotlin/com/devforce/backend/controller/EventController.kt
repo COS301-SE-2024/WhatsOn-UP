e4c1a7e13b099298624dd47236c667ae19579245
Nokukhanya Ndlovu <135546695+NokukhanyaNdlovu@users.noreply.github.com>
2024-06-20 14:16:53 +0200

    conflict changes

--- src/Backend/src/main/kotlin/com/devforce/backend/controller/EventController.kt
+++ src/Backend/src/main/kotlin/com/devforce/backend/controller/EventController.kt
@@ -4,24 +4,27 @@ import com.devforce.backend.dto.CreateEventDto
 import com.devforce.backend.dto.ResponseDto
 import com.devforce.backend.dto.UpdateEventDto
 import com.devforce.backend.service.EventService
+import jakarta.annotation.security.RolesAllowed
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 import java.util.UUID
 
 @RestController
-@RequestMapping("/events")
+@RequestMapping("/api/events")
 class EventController {
 
     @Autowired
     lateinit var eventService: EventService
 
     @PostMapping("/create")
-    @PreAuthorize("hasAnyRole('ADMIN', 'HOST')")
-    fun createEvent(@RequestBody event: CreateEventDto): ResponseEntity<ResponseDto> {
-        return eventService.createEvent(event)
+    @RolesAllowed("HOST", "ADMIN")
+    fun createEvent(@RequestBody event: CreateEventDto, @RequestHeader("Authorization") token: String): ResponseEntity<ResponseDto> {
+        val jwtToken = token.substring(7)
+        return eventService.createEvent(event, jwtToken)
     }
 
     @GetMapping("/get_all")
@@ -31,13 +34,13 @@ class EventController {
     }
 
     @PutMapping("/update/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN', 'HOST')")
+    @RolesAllowed("HOST", "ADMIN")
     fun updateEvent(@PathVariable id: UUID, @RequestBody event: UpdateEventDto): ResponseEntity<ResponseDto> {
         return eventService.updateEvent(id, event)
     }
 
     @DeleteMapping("/delete/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN', 'HOST')")
+    @RolesAllowed("HOST", "ADMIN")
     fun deleteEvent(@PathVariable id: UUID): ResponseEntity<ResponseDto> {
         return eventService.deleteEvent(id)
     }
@@ -59,8 +62,8 @@ class EventController {
     fun searchEvents(
         @RequestParam(required = false) title: String?,
         @RequestParam(required = false) description: String?,
-        @RequestParam(required = false) startDate: Long?,
-        @RequestParam(required = false) endDate: Long?
+        @RequestParam(required = false) startDate: LocalDateTime?,
+        @RequestParam(required = false) endDate: LocalDateTime?
     ): ResponseEntity<ResponseDto> {
         return eventService.searchEvents(title, description, startDate, endDate)
     }
--- src/Backend/src/main/kotlin/com/devforce/backend/controller/UserController.kt
+++ src/Backend/src/main/kotlin/com/devforce/backend/controller/UserController.kt
@@ -10,51 +10,59 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 class UserController {
     @Autowired
     lateinit var userService: UserService
 
     @PutMapping("/save_event/{id}")
     @PreAuthorize("isAuthenticated()")
-    fun saveEvent(@PathVariable id: UUID): ResponseEntity<ResponseDto> {
-        return userService.saveEvent(id)
+    fun saveEvent(@PathVariable id: UUID, @RequestHeader("Authorization") token: String): ResponseEntity<ResponseDto> {
+        val jwtToken = token.substring(7)
+        return userService.saveEvent(id, jwtToken)
     }
 
     @DeleteMapping("/delete_saved_event/{id}")
     @PreAuthorize("isAuthenticated()")
-    fun deleteSavedEvent(@PathVariable id: UUID): ResponseEntity<ResponseDto> {
-        return userService.deleteSavedEvent(id)
+    fun deleteSavedEvent(@PathVariable id: UUID, @RequestHeader("Authorization") token: String): ResponseEntity<ResponseDto> {
+        val jwtToken = token.substring(7)
+        return userService.deleteSavedEvent(id, jwtToken)
     }
 
     @GetMapping("/get_saved_events")
     @PreAuthorize("isAuthenticated()")
-    fun getSavedEvents(): ResponseEntity<ResponseDto> {
-        return userService.getSavedEvents()
+    fun getSavedEvents(@RequestHeader("Authorization") token: String): ResponseEntity<ResponseDto> {
+        val jwtToken = token.substring(7)
+        return userService.getSavedEvents(jwtToken)
     }
 
 
     @PutMapping("rspv_event/{id}")
     @PreAuthorize("isAuthenticated()")
-    fun rspvEvent(@PathVariable id: UUID): ResponseEntity<ResponseDto> {
-        return userService.rspvEvent(id)
+    fun rspvEvent(@PathVariable id: UUID,@RequestHeader("Authorization") token: String): ResponseEntity<ResponseDto> {
+        val jwtToken = token.substring(7)
+        return userService.rspvEvent(id, jwtToken)
     }
 
     @GetMapping("/get_rspv_events")
     @PreAuthorize("isAuthenticated()")
-    fun getRspvEvents(): ResponseEntity<ResponseDto> {
-        return userService.getRspvEvents()
+    fun getRspvEvents(@RequestHeader("Authorization") token: String): ResponseEntity<ResponseDto> {
+        val jwtToken = token.substring(7)
+        return userService.getRspvEvents(jwtToken)
     }
 
     @DeleteMapping("/delete_rspv_event/{id}")
     @PreAuthorize("isAuthenticated()")
-    fun deleteRspvEvent(@PathVariable id: UUID): ResponseEntity<ResponseDto> {
-        return userService.deleteRspvEvent(id)
+    fun deleteRspvEvent(@PathVariable id: UUID, @RequestHeader("Authorization") token: String): ResponseEntity<ResponseDto> {
+        val jwtToken = token.substring(7)
+        return userService.deleteRspvEvent(id, jwtToken)
     }
 
     @PutMapping("/update_profile")
     @PreAuthorize("isAuthenticated()")
-    fun updateProfile(@RequestBody user: UpdateUserDto): ResponseEntity<ResponseDto> {
-        return userService.updateProfile(user)
+    fun updateProfile(@RequestBody userDto: UpdateUserDto, @RequestHeader("Authorization") token: String): ResponseEntity<ResponseDto> {
+        val jwtToken = token.substring(7)
+        return userService.updateProfile(userDto, jwtToken)
     }
+
 }
