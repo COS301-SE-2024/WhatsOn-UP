a514c0597dda7e11827c6f40c82c5a867b5e3689
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2024-06-21 13:11:18 +0200

    Add integration for calendar page - Also added default theme in theme_manager to prevent errors on app startup

--- src/Frontend/lib/services/api.dart
+++ src/Frontend/lib/services/api.dart
@@ -77,4 +77,29 @@ class Api {
       throw Exception(e.toString());
     }
   }
+
+
+  //Method to retrieve rsvpd events
+  Future<List<dynamic>> getRSVPEvents() async {
+    try {
+      final String _rsvpEventsURL = 'http://localhost:8080/api/user/get_rspv_events';
+      var headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $jwtKey',
+      };
+
+      var response = await http.get(Uri.parse(_rsvpEventsURL), headers: headers);
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body)['data'];
+      } else {
+        throw Exception(jsonDecode(response.body));
+      }
+    }
+    catch (e) {
+      print('Error: $e');
+      throw Exception(e.toString());
+    }
+  }
 }
