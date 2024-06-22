0f2ded0e7939e2479823edd690666154cb3c6001
Nokukhanya Ndlovu <135546695+NokukhanyaNdlovu@users.noreply.github.com>
2024-06-22 03:45:11 +0200

    Add festivall.jpg image file

---
+++ src/Frontend/lib/widgets/FilteredEventsScreen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class FilteredEventsScreen extends StatelessWidget {
+  final List<dynamic> events;
+
+  FilteredEventsScreen({required this.events});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filtered Events'),
+      ),
+      body: ListView.builder(
+        itemCount: events.length,
+        itemBuilder: (context, index) {
+          final event = events[index];
+          return ListTile(
+            title: Text(event['title']),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(event['description']),
+                Text('Location: ${event['location']}'),
+                Text('Start Time: ${event['start_time']}'),
+                Text('End Time: ${event['end_time']}'),
+              ],
+            ),
+            // Implement onTap if needed
+          );
+        },
+      ),
+    );
+  }
+}
