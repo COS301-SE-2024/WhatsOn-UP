27df4b8f5767cf4e42f42bce1e8b69de2a02998b
KhweziN <khwezisandi@gmail.com>
2024-06-24 00:59:18 +0200

    changed filterEvents to return a list of Event objects

--- src/Frontend/lib/services/EventService.dart
+++ src/Frontend/lib/services/EventService.dart
@@ -57,8 +57,12 @@ class EventService {
     try {
       final response = await http.get(uri);
       if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body)['data'];
-        return data;
+        final Map<String, dynamic> decodedJson = json.decode(response.body);
+        final List<dynamic> eventsJson = decodedJson['data'];
+
+        // Map the JSON objects to Event objects
+        final List<Event> events = eventsJson.map((jsonEvent) => Event.fromJson(jsonEvent)).toList();
+        return events;
       } else {
         throw Exception('Failed to filter events');
       }
