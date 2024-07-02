c01df7c8e1efb72ddc8a05d84974ff6d24f91c83
zdanise <zetdanise89@gmail.com>
2024-07-02 19:39:05 +0200

    login and signup supabase and confirmation email

---
+++ src/Frontend/lib/pages/supabase_Accountpage.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../main.dart';
+class SupabaseAccountpage extends StatelessWidget {
+  const SupabaseAccountpage({super.key});
+
+
+  void _logout(BuildContext context) async {
+    // Call Supabase signOut method to clear the session
+    await supabase.auth.signOut();
+
+    // Navigate to the login page
+    Navigator.of(context).pushReplacementNamed('/login');
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Account'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _logout(context),
+          child: const Text('Logout'),
+        ),
+      ),
+    );
+  }
+}
