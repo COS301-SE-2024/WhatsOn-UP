b429a69f0e733d3873dba7cd13c390da43d3873e
KhweziN <khwezisandi@gmail.com>
2024-07-23 10:33:04 +0200

    added basic navigation functionality

--- src/Frontend/lib/pages/explore_page.dart
+++ src/Frontend/lib/pages/explore_page.dart
@@ -1,30 +1,197 @@
+import 'package:firstapp/models/Route.dart';
+import 'package:firstapp/services/RouteService.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mapbox_navigation/flutter_mapbox_navigation.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class ExplorePage extends StatelessWidget {
-  const ExplorePage({super.key});
+class NavigationPage extends StatefulWidget {
+  @override
+  _NavigationPageState createState() => _NavigationPageState();
+}
+
+// class _NavigationPageState extends State<NavigationPage> {
+//   late MapBoxNavigation _directions;
+//   late MapBoxOptions _options;
+//   late MapBoxNavigationViewController _controller;
+
+//   String _instruction = "";
+//   bool _arrived = false;
+//   bool _routeBuilt = false;
+//   bool _isNavigating = false;
+//   double _distanceRemaining = 0.0;
+//   double _durationRemaining = 0.0;
+//   bool _isMultipleStop = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _directions = MapBoxNavigation();
+//     _options = MapBoxOptions(
+//       initialLatitude: 37.7749,
+//       initialLongitude: -122.4194,
+//       zoom: 15.0,
+//       tilt: 0.0,
+//       bearing: 0.0,
+//       enableRefresh: false,
+//       alternatives: true,
+//       voiceInstructionsEnabled: true,
+//       bannerInstructionsEnabled: true,
+//       longPressDestinationEnabled: true,
+//       mode: MapBoxNavigationMode.driving,
+//       simulateRoute: false,
+//       language: "en",
+//       units: VoiceUnits.metric,
+//     );
+
+//     MapBoxNavigation.instance.registerRouteEventListener(_onRouteEvent);
+//   }
+
+//   Future<void> _onRouteEvent(e) async {
+//     _distanceRemaining = await _directions.getDistanceRemaining() as double;
+//     _durationRemaining = await _directions.getDurationRemaining() as double;
+
+//     switch (e.eventType) {
+//       case MapBoxEvent.progress_change:
+//         var progressEvent = e.data as RouteProgressEvent;
+//         _arrived = progressEvent.arrived as bool;
+//         if (progressEvent.currentStepInstruction != null)
+//           _instruction = progressEvent.currentStepInstruction as String;
+//         break;
+//       case MapBoxEvent.route_building:
+//       case MapBoxEvent.route_built:
+//         _routeBuilt = true;
+//         break;
+//       case MapBoxEvent.route_build_failed:
+//         _routeBuilt = false;
+//         break;
+//       case MapBoxEvent.navigation_running:
+//         _isNavigating = true;
+//         break;
+//       case MapBoxEvent.on_arrival:
+//         _arrived = true;
+//         if (!_isMultipleStop) {
+//           await Future.delayed(Duration(seconds: 3));
+//           await _directions.finishNavigation();
+//         } else {}
+//         break;
+//       case MapBoxEvent.navigation_finished:
+//       case MapBoxEvent.navigation_cancelled:
+//         _routeBuilt = false;
+//         _isNavigating = false;
+//         break;
+//       default:
+//         break;
+//     }
+//     setState(() {});
+//   }
+
+//   Future<void> startNavigation() async {
+//     var wayPoints = <WayPoint>[];
+//     wayPoints.add(WayPoint(name: "start", latitude: 37.7749, longitude: -122.4194));
+//     wayPoints.add(WayPoint(name: "end", latitude: 37.7849, longitude: -122.4094));
+
+//     await _directions.startNavigation(
+//       wayPoints: wayPoints,
+//       options: _options,
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Navigation")),
+//       body: Container(
+//                 color: Colors.grey,
+//                 child: MapBoxNavigationView(
+//                     options: _options,
+//                     onRouteEvent: _onRouteEvent,
+//                     onCreated:
+//                         (MapBoxNavigationViewController controller) async {
+//                       _controller = controller;
+//                     }),
+//               ),
+//     );
+//   }
+// }
+
+class _NavigationPageState extends State<NavigationPage> {
+  late GoogleMapController _googleMapController;
+  static const LatLng initPos = LatLng(-25.756283, 28.231191);
+  Marker? _origin;
+  Marker? _destination;
+  MapRoute? _route;
+
+  static const _initialCameraPosition = CameraPosition(
+    target: initPos,
+    zoom: 17,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _origin = Marker(
+          markerId: const MarkerId("origin"),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          position: initPos
+        );
+  }
+
+  @override
+  void dispose() {
+    _googleMapController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-                'Explore',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-          ),
-          SizedBox(height: 350,),
-          Center(
-            child: Text('Explore Page - Coming soon'),
-          ),
-        ]
-      )
+    
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _initialCameraPosition,
+        onMapCreated: (controller) => _googleMapController = controller,
+        markers: {
+          if (_origin != null) _origin as Marker,
+          if (_destination != null) _destination as Marker
+        },
+        polylines:{
+          if (_route != null)
+            Polyline(
+              polylineId: const PolylineId('overview_polyline'),
+              color: Colors.red,
+              width: 5,
+              points: _route!.polylinePoints
+                  .map((e) => LatLng(e.latitude, e.longitude))
+                  .toList(),
+            ),
+        },
+        onLongPress: _setDestination,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: Colors.white,
+        onPressed: () => _googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(_initialCameraPosition),
+        ),
+        child: const Icon(Icons.center_focus_strong)
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartDocked,
     );
   }
+
+  void _setDestination(LatLng _position) async {
+    setState(() {
+        _destination = Marker(
+          markerId: const MarkerId("destination"),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          position: _position
+        );
+      });
+    
+    final updatedRoute = await RouteService().getRoute(origin: _origin!.position, destination: _position);
+    setState(() {
+      _route = updatedRoute;
+    });
+    print('\n ${updatedRoute}');
+  }
+
 }
