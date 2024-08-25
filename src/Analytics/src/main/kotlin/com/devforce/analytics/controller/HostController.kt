0d67218f713d5c24841e59ee6ec8042493c4ec11
Ms.P <u22524798@tuks.co.za>
2024-08-25 22:59:03 +0200

    chore: Add delete endpoints for notifications

--- src/Analytics/src/main/kotlin/com/devforce/analytics/controller/HostController.kt
+++ src/Analytics/src/main/kotlin/com/devforce/analytics/controller/HostController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/analytics/host")
 @PreAuthorize("hasAnyRole('HOST')")
 class HostController {
 
