6153a3b149dcd1cc16259af15cc4fa1be45368f0
Ms.P <u22524798@tuks.co.za>
2024-09-04 09:26:29 +0200

    Deployed analytics

--- src/Analytics/src/main/kotlin/com/devforce/analytics/security/AuthFilter.kt
+++ src/Analytics/src/main/kotlin/com/devforce/analytics/security/AuthFilter.kt
@@ -1,25 +1,43 @@
 package com.devforce.analytics.security
 
+import com.devforce.backend.dto.ResponseDto
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.ExpiredJwtException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
-
+import java.security.SignatureException
+import java.util.Date
 
 @Component
-class AuthFilter: OncePerRequestFilter() {
+class AuthFilter : OncePerRequestFilter() {
+
+    @Value("\${SECRET}")
+    private val SECRET: String? = null
 
     @Autowired
     private lateinit var customUserDetailsService: CustomUserDetailsService
 
-
+    private val ENDPOINTS: List<String> = listOf(
+        "/api/events/get_all",
+        "/api/events/filter",
+        "/api/events/search",
+        "/api/events/categories",
+        "/api/events/filterEvents",
+        "/api/events/get_locations",
+        "/api/user/verify_application"
+    )
 
     @Throws(ServletException::class, IOException::class)
     override fun doFilterInternal(
@@ -27,19 +45,55 @@ class AuthFilter: OncePerRequestFilter() {
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
+        val requestURI = request.requestURI
+        val jwt = getBearer(request)
+        val match = ENDPOINTS.any { suffix -> requestURI.endsWith(suffix) }
 
-        val id = getBearer(request)
-        if (id == null) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid user ID")
+        if (match && jwt == null) {
+            filterChain.doFilter(request, response)
             return
         }
-        try {
-            val userDetails: CustomUser = customUserDetailsService.loadUserByUsername(id)
-            val authenticationToken = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
-            authenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
-            SecurityContextHolder.getContext().authentication = authenticationToken
-        } catch (e: Exception) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid user ID")
+
+        if (jwt != null) {
+            try {
+                val claims = getUserIdFromToken(jwt)
+                val userId = claims.subject
+                if (claims.expiration.before(Date())) {
+                    response.status = HttpServletResponse.SC_UNAUTHORIZED
+                    response.contentType = "application/json"
+                    val responseDto = ResponseDto("error", Date().time, "Token expired")
+                    response.writer.write(ObjectMapper().writeValueAsString(responseDto))
+                    return
+                }
+
+                val userDetails = customUserDetailsService.loadUserByUsername(userId)
+                val authenticationToken = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
+                authenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+                SecurityContextHolder.getContext().authentication = authenticationToken
+            } catch (e: ExpiredJwtException) {
+                response.status = HttpServletResponse.SC_UNAUTHORIZED
+                response.contentType = "application/json"
+                val responseDto = ResponseDto("error", Date().time, "Token expired")
+                response.writer.write(ObjectMapper().writeValueAsString(responseDto))
+                return
+            } catch (e: SignatureException) {
+                response.status = HttpServletResponse.SC_UNAUTHORIZED
+                response.contentType = "application/json"
+                val responseDto = ResponseDto("error", Date().time, "Invalid token signature")
+                response.writer.write(ObjectMapper().writeValueAsString(responseDto))
+                return
+            } catch (e: Exception) {
+                response.status = HttpServletResponse.SC_UNAUTHORIZED
+                response.contentType = "application/json"
+                val responseDto = ResponseDto("error", Date().time, "Invalid token")
+                response.writer.write(ObjectMapper().writeValueAsString(responseDto))
+                return
+            }
+        } else {
+            response.status = HttpServletResponse.SC_UNAUTHORIZED
+            response.contentType = "application/json"
+            val responseDto = ResponseDto("error", Date().time, "Token not provided")
+            response.writer.write(ObjectMapper().writeValueAsString(responseDto))
             return
         }
 
@@ -53,4 +107,10 @@ class AuthFilter: OncePerRequestFilter() {
         } else null
     }
 
+    fun getUserIdFromToken(jwtToken: String): Claims {
+        return Jwts.parser()
+            .setSigningKey(SECRET?.toByteArray())
+            .parseClaimsJws(jwtToken)
+            .body
+    }
 }
