7077868ee0ec99ab7cd745c993b2fe5a410ef233
KhweziN <khwezisandi@gmail.com>
2024-09-08 00:47:38 +0200

    added navbar to documentation

--- docs/VuePress/docs/.vuepress/config.js
+++ docs/VuePress/docs/.vuepress/config.js
@@ -4,5 +4,25 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  theme: defaultTheme({
+    logo: "/images/Logo.png",
+    navbar: [
+      {
+        text: 'Home',
+        link: '/',
+      },
+      {
+        text: 'Documentation',
+        link: '/',
+      },
+      {
+        text: 'Meet The Team',
+        link: '/',
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/COS301-SE-2024/WhatsOn-UP',
+      },
+    ],
+  }),
 })
