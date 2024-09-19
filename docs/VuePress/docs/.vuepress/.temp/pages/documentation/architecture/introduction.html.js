1e703826f0db8bc82a48444e29eb1c6e04c2d3a5
Nokukhanya Ndlovu <135546695+NokukhanyaNdlovu@users.noreply.github.com>
2024-09-19 22:56:37 +0200

    documentation changes

---
+++ docs/VuePress/docs/.vuepress/.temp/pages/documentation/architecture/introduction.html.js
@@ -0,0 +1,16 @@
+import comp from "C:/Users/khany/Documents/GitHub/WhatsOn-UP/docs/VuePress/docs/.vuepress/.temp/pages/documentation/architecture/introduction.html.vue"
+const data = JSON.parse("{\"path\":\"/documentation/architecture/introduction.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Introduction of Project\",\"slug\":\"introduction-of-project\",\"link\":\"#introduction-of-project\",\"children\":[]}],\"git\":{\"updatedTime\":1726359177000,\"contributors\":[{\"name\":\"zdanise\",\"email\":\"zetdanise89@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"documentation/architecture/introduction.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
