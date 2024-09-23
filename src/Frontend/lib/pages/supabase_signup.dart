834a5c28edcb40d7915ee300b1680aaf965b5fdf
zdanise <zetdanise89@gmail.com>
2024-09-23 16:08:28 +0200

    survey obstructed by JWT

--- src/Frontend/lib/pages/supabase_signup.dart
+++ src/Frontend/lib/pages/supabase_signup.dart
@@ -28,22 +28,22 @@ class _SupabaseSignupState extends State<SupabaseSignup> {
   late Color myColor;
   late Size mediaSize;
 
-  @override
-  void initState() {
-    super.initState();
-    _authSubscription = supabase.auth.onAuthStateChange.listen((event) {
-      final session = event.session;
-      if (session != null) {
-        Navigator.of(context).pushReplacementNamed('/login');
-      }
-    });
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _authSubscription = supabase.auth.onAuthStateChange.listen((event) {
+  //     final session = event.session;
+  //     if (session != null) {
+  //       Navigator.of(context).pushReplacementNamed('/login');
+  //     }
+  //   });
+  // }
 
   @override
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
-    _authSubscription.cancel();
+    // _authSubscription.cancel();
     super.dispose();
   }
 
@@ -154,10 +154,11 @@ class _SupabaseSignupState extends State<SupabaseSignup> {
                     .signUp(email: email, password: password);
                 if (mounted) {
                   print("CALLING USERNAME INPUT");
+                  Provider.of<userProvider>(context, listen: false).JWT =
+                      supabase.auth.currentSession!.accessToken;
                   await _usernameInput(); // Ensure the username is saved
                   ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text("Signed up successfully")));
-                  Navigator.of(context).pushReplacementNamed('/login');
                 }
               } on AuthException catch (error) {
                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(
