21a4a51410a79582dc1cba18aecf7d2efa62896a
Brenden1998 <50359806+Brenden1998@users.noreply.github.com>
2024-09-23 12:31:14 +0200

    Improve no data message for past events

--- src/Frontend/lib/widgets/Pastevents.dart
+++ src/Frontend/lib/widgets/Pastevents.dart
@@ -49,11 +49,11 @@ class _PasteventsState extends State<Pastevents> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Past Events'),
+        title: const Text('Past Events'),
       ),
       body: Column(
         children: [
-          SizedBox(width: 35.0),
+          const SizedBox(width: 35.0),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Center(
@@ -81,7 +81,7 @@ class _PasteventsState extends State<Pastevents> {
                       ),
                     ),
                   ),
-                  SizedBox(width: 35.0),
+                  const SizedBox(width: 35.0),
                   SizedBox(
                     width: MediaQuery.of(context).size.width * 0.27,
                     child: Container(
@@ -107,22 +107,41 @@ class _PasteventsState extends State<Pastevents> {
               ),
             ),
           ),
-          SizedBox(width: 35.0),
+          const SizedBox(width: 35.0),
           Expanded(
             child: FutureBuilder<List<Event>>(
               future: _pastEvents,
               builder: (context, AsyncSnapshot<List<Event>> snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
+                  return const Center(
                     child: SpinKitPianoWave(
                       color: Color.fromARGB(255, 149, 137, 74),
                       size: 50.0,
                     ),
                   );
                 } else if (snapshot.hasError) {
-                  return Center(child: Text('Error loading events'));
+                  return const Center(child: Text('Error loading events'));
                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return Center(child: Text('No events available'));
+                    return const Center(
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.info_outline, size: 64, color: Color.fromARGB(255, 119, 119, 119),),
+                          SizedBox(height: 16),
+                          Center(
+                            child: Text(
+                              'No Events Available',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 119, 119, 119),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );  
                 } else {
                   List<Event> events = snapshot.data!;
                   DateTime now = DateTime.now();
